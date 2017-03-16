55a08cffd4476909ef2e20d5c8b4c4aa2259d314
Benjamin T. Seaver and Charles Peden <benjamin@>
2017-03-16 14:48:13 -0700

    Refactor (js/frontend/search-interface.js)

--- js/frontend/search-interface.js
+++ js/frontend/search-interface.js
@@ -6,24 +6,43 @@ var newResult;
 
 var displayResults = function(searchResult) {
   newResult = searchResult;
-  var displayResult;
-  if (newResult.status == "OK") {
-    if(newResult.response.docs.length === 0){
-      displayResult = '<div class="well"><p>' + newResult.copyright + '</p><p>No Results Found</p></div>';
-    } else {
-      displayResult = '<div class="well"><p>' + newResult.copyright + '</p></div>';
-      newResult.response.docs.forEach(function(doc){
-        displayResult += '<div class="well">';
-        displayResult += '<p><a href="' + doc.web_url + '" target="_blank">' + doc.headline.main + '</a></p>';
-        displayResult += '<p>' + doc.lead_paragraph + '</p>';
-        displayResult += '<p>' + doc.byline.original + '</p>';
-        displayResult += '<p>' + doc.pub_date.slice(0,10) + '</p>';
-        displayResult += '</div>';
-      });
-    }
+  done = false;
+  var displayResult = "";
+
+  if (!done && newResult.status != "OK") {
+    displayResult += '<div class="well"><p>Returned Status: ' + newResult.status + '</p></div>';
+    done = true;
   }
+
+  if (!done && newResult.response.docs.length === 0 && newResult.response.meta.offset <= 0) {
+    displayResult = '<div class="well"><p>' + newResult.copyright + '</p><p>No Results Found</p></div>';
+    done = true;
+  }
+
+  if (!done && newResult.response.docs.length === 0 && newResult.response.meta.offset > 0) {
+    displayResult += '<div class="well"><p>No further results returned</p></div>';
+    done = true;
+  }
+
+  if (!done && newResult.response.docs.length > 0 && newResult.response.meta.offset <= 0) {
+    displayResult += '<div class="well"><p>' + newResult.copyright + '</p></div>';
+  }
+
+  if (!done && newResult.response.docs.length > 0) {
+    newResult.response.docs.forEach(function(doc){
+      displayResult += '<div class="well">';
+      displayResult += '<p><a href="' + doc.web_url + '" target="_blank">' + doc.headline.main + '</a></p>';
+      displayResult += '<p>' + doc.lead_paragraph + '</p>';
+      if ("byline" in doc && doc.byline != null && "original" in doc.byline) {
+        displayResult += '<p>' + doc.byline.original + '</p>';
+      }
+      displayResult += '<p>' + doc.pub_date.slice(0,10) + '</p>';
+      displayResult += '</div>';
+    });
+  }
+
   if (displayResult) {
-    $("#searchOutput").html(displayResult);
+    $("#searchOutput").append(displayResult);
   }
 };
 
@@ -51,13 +70,11 @@ var displayFailure = function(searchResult){
 
   displayResult = '<div class="well"><p>Search failed.</p>' + displayResult + '</div>';
 
-  $("#searchOutput").html(displayResult);
+  $("#searchOutput").append(displayResult);
 };
 
 
 $(document).ready(function(){
-  var win = $(window);
-  var newSearch;
   $("#search_terms").submit(function(event) {
     event.preventDefault();
     var allTheWords = $("#all-the-words").val();
@@ -66,13 +83,24 @@ $(document).ready(function(){
     var withoutWords = $("#without-words").val();
 
     // displayResults(searchSample);
+    $("#searchOutput").empty();
     newSearch = new Search(allTheWords, exactPhrase, atLeastOne, withoutWords);
     newSearch.searchNYTimes(0, displayResults, displayFailure);
   });
+
+  var win = $(window);
   win.scroll(function(){
     if($(document).height() - win.height() == win.scrollTop()){
       console.log("end of document reached.");
-
+      if (typeof newResult != "object") {
+        return;
+      }
+      if (newResult.response.docs.length < 10) {
+        return;
+      }
+      var nextPage = Math.floor(newResult.response.meta.offset / 10) + 1;
+      console.log(nextPage);
+      newSearch.searchNYTimes(nextPage, displayResults, displayFailure);
     }
   });
 });
